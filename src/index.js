071030ebd1786f25ccad8479c4461d22f9d545cf
SaiPradeepti <saipradeepti14@gmail.com>
2020-11-23 01:44:26 +0530

    JSX JavaScript

--- src/index.js
+++ src/index.js
@@ -17,19 +17,17 @@ function BookList(){
     </section>);
 }
 
+const author = 'Amelia Hepworth';
 const Book = () => {
+  const title = 'I Love You to the Moon and Back';
   return (
   <article class="book">
-    <Image/>    
-    <Title />
-    <Author />
+    <img src="https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UY218_.jpg" alt=""/> 
+    <h1>{title}</h1>
+  <h4>{author.toUpperCase()}</h4>
+  <p>{6+6}</p>
   </article>);
 }
 
-const Image = () => <img src="https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UY218_.jpg" alt=""/>
-
-const Title = () => <h1>I Love You to the Moon and Back </h1>;
-
-const Author = () => <h4 style={{color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem'}}>Amelia Hepworth</h4>;
 
 ReactDom.render(<BookList/>,document.getElementById('root'));
