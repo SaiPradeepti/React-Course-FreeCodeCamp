be9c7f5fea957aa4f9e75f3146ed857e2f37c03f
SaiPradeepti <saipradeepti14@gmail.com>
2020-11-22 17:53:24 +0530

    Nested Components

--- src/index.js
+++ src/index.js
@@ -1,26 +1,20 @@
 import React from 'react';      // to create component we need to import react
 import ReactDom from 'react-dom'; //
 
-// JSX Rules
-// JSX must return single value
-// return div/ section/ article/ Fragment
-// Use camelCase for html attributes
-// className instead of class
-// close every element....meaning that for even self closing tags use slash </> like <img src="" />
-// formatting .. add parenthesis while returning otherwise it is necessary to have opening tag on the line of return
+// Nested Components and React Tools
 
-const NavBarComponent = () => {
+function Greeting(){
   return (
-    <>
-      <div className="nav-body">
-        <div className="logo">
-          <a href='#'>
-            Logo
-          </a>
-        </div>
-      </div>
-    </>
+    <div>
+      <Greet/>
+      <Person/>
+    </div>
   );
 }
+// implicit return
+const Person = () => <h3>This is deepti</h3>;
 
-ReactDom.render(<NavBarComponent/>, document.getElementById('root'));
+// explicit return 
+const Greet = () => {return <h1>Hello World!</h1>};
+
+ReactDom.render(<Greeting/>,document.getElementById('root'));
