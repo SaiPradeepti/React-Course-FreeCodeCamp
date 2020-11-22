52061afec3974d01527919eecf2797fc63999e8a
SaiPradeepti <saipradeepti14@gmail.com>
2020-11-22 20:22:50 +0530

    Mini Book Project

--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
-import React from 'react';      // to create component we need to import react
-import ReactDom from 'react-dom'; //
+import React from 'react';
+import ReactDom from 'react-dom';
 
-// Nested Components and React Tools
-
-function Greeting(){
+function BookList(){
   return (
-    <div>
-      <Greet/>
-      <Person/>
-    </div>
-  );
+    <section>
+      <Book />
+    </section>);
 }
-// implicit return
-const Person = () => <h3>This is deepti</h3>;
 
-// explicit return 
-const Greet = () => {
-  return <h1>Hello World!</h1>
-};
+const Book = () => {
+  return (
+  <article>
+    <Image/>    
+    <Title />
+    <Author />
+  </article>);
+}
 
-ReactDom.render(<Greeting/>,document.getElementById('root'));
+const Image = () => <img src="https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UY218_.jpg" alt=""/>
+
+const Title = () => <h1>I Love You to the Moon and Back </h1>;
+
+const Author = () => <h4>Amelia Hepworth</h4>;
+
+ReactDom.render(<BookList/>,document.getElementById('root'));
