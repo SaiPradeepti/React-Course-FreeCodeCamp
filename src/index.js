25cb9f34f016dd393f4df55e6bb9a58dd5a79340
SaiPradeepti <saipradeepti14@gmail.com>
2020-11-24 15:18:42 +0530

    Adding Props

--- src/index.js
+++ src/index.js
@@ -4,28 +4,42 @@ import ReactDom from 'react-dom';
 // CSS
 import './index.css'
 
+// setup vars
+const bookDetails = [
+  {
+    author: 'Amelia Hepworth',
+    title: 'I Love You to the Moon and Back',
+    img: 'https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UY218_.jpg'
+  },
+  {
+    author: 'Barack Obama',
+    title: 'A Promised Land',
+    img: 'https://images-eu.ssl-images-amazon.com/images/I/91Cz5v-GSjL._AC_UL200_SR200,200_.jpg'
+  },
+  {
+    author: 'Paulo Coelho',
+    title: 'The Alchemist',
+    img: 'https://images-eu.ssl-images-amazon.com/images/I/71aFt4%2BOTOL._AC_UL200_SR200,200_.jpg'
+  }
+];
+
 function BookList(){
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book img = {bookDetails[0].img} title = {bookDetails[0].title} author = {bookDetails[0].author} />
+      <Book img = {bookDetails[1].img} title = {bookDetails[1].title} author = {bookDetails[1].author} />
+      <Book img = {bookDetails[2].img} title = {bookDetails[2].title} author = {bookDetails[2].author} />
     </section>);
 }
 
-const author = 'Amelia Hepworth';
-const Book = () => {
-  const title = 'I Love You to the Moon and Back';
+const Book = (props) => {
+  console.log(props);
+  const {author, title, img} = props;
   return (
-  <article class="book">
-    <img src="https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UY218_.jpg" alt=""/> 
+  <article className="book">
+    <img src={img} alt=""/> 
     <h1>{title}</h1>
-  <h4>{author.toUpperCase()}</h4>
-  <p>{6+6}</p>
+    <h4>{author}</h4>
   </article>);
 }
 
