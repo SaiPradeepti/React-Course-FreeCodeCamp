a8afc19e63be4789b957c6a951efc41e80278ece
SaiPradeepti <saipradeepti14@gmail.com>
2020-11-24 22:55:58 +0530

    Event basics

--- src/index.js
+++ src/index.js
@@ -34,14 +34,23 @@ function BookList(){
   );
 }
 
-// const Book = ({author, title, img}) => {
-const Book = (props) => {
-  const {author, title, img} = props;
+const Book = ({author, title, img}) => {
+  // attribute, eventHandler
+  // onClick, onMouse
+  const addItem = (e) => {
+    console.log(e.target);
+    alert('Item added to cart!');
+  }
+  const removeItem = (title, author) => {
+    alert(`Item '${title} (by ${author})' removed from cart!`);
+  }
   return (
   <article className="book">
     <img src={img} alt=""/> 
     <h1>{title}</h1>
     <h4>{author}</h4>
+    <button type="button" onClick={addItem}>Add to cart</button>
+    <button type="button" onClick={() => removeItem(title, author)}>Delete from cart</button>
   </article>);
 }
 
